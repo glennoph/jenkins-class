df0c2e895faddb97ce6420d53b190a7787fd9d6a
glenn opdycke-hansen <glennoph@gmail.com>
2022-05-28 10:53:42 -0500

    jenkins build & start, challenge 3

---
+++ jenkins-build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo 'jenkins-build docker image tag: jenins_local'
+
+docker build -t jenkins_local .
+
---
+++ jenkins-start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo 'jenkins-start docker image name jenins_local'
+
+docker run -d --name jenkins_local jenkins_local
+
